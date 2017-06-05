ccb503d2688bd3e401f61ee196f31e7da17482c9
antonio_f <felices.antonio@yahoo.es>
2017-06-05 14:32:49 +0200

    deleted requirejs, efects, update node, add browserSync, etc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,158 +1,157 @@
 /*
- *
- * Gruntfile moare presentations Bootstrap
- *
- */
+*
+* Gruntfile moare presentations Bootstrap
+*
+*/
 
 'use strict';
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    //Bower
-    bower:{
-      install: {
-        options: {
-          targetDir: 'app/assets/js',
-          layoud: 'byComponent',
-          install: true,
-          cleanBowerDir: true
-        }
-      }
-    },
-
-    //Compass
-    compass: {
-      dev: {
-        options: {
-          httpPath: '/',
-          cssDir: 'app/assets/stylesheets',
-          sourcemap: true,
-          sassDir: 'app/assets/scss',
-          imagesDir: 'app/assets/images',
-          javascriptsDir: 'app/assets/js',
-          fontsDir: 'app/assets/fonts',
-          outputStyle: 'expanded',
-          environment: 'development'
-        }
-      },
-      prod: {
-        options: {
-          httpPath: '/',
-          sassDir:'app/assets/scss',
-          specify: 'app/assets/scss/main.scss',
-          cssDir:'dist/assets/stylesheets',
-          sourcemap: true,
-          outputStyle: 'compressed',
-          environment: 'production'
-        }
-      }
-    },
-
-    //Uglify
-    uglify: {
-      options: {
-        preserveComments: false,
-      },
-      all: {
-        files: {
-          'dist/assets/js/require.min.js': ['app/assets/js/lib/requirejs/require.js']
-        }
-      }
-    },
-
-    //Watch
-    watch: {
-      css: {
-        files: ['app/assets/scss/*.scss','app/assets/scss/plugins/*.scss'],
-        tasks: ['compass:dev']
-      }
-    },
-    
-    //Copy
-    copy: {
-      main: {
-        files: [{
-          expand: true,
-          cwd: 'app/',
-          src: ['*.html'],
-          dest: 'dist/'
-        }]
-      },
-      images: {
-        files: [{
-          expand: true,
-          cwd: 'app/assets/imgs',
-          src: ['*.{png,jpg,svg,gif}'],
-          dest: 'dist/assets/imgs'
-        }]
-      },
-      imagescss: {
-        files: [{
-          expand: true,
-          cwd: 'app/assets/imgs/css',
-          src: ['*.{png,jpg,svg,gif}'],
-          dest: 'dist/assets/imgs/css'
-        }]
-      },
-      fonts:{
-        files: [{
-          expand: true,
-          cwd: 'app/assets/fonts',
-          src: ['**/*.*'],
-          dest: 'dist/assets/fonts'
-        }]
-      },
-    },
-
-    //Replace
-    'string-replace': {
-      inline: {
-        files: [{
-          expand: true,
-          cwd: 'dist/',
-          src: '*.html',
-          dest: 'dist/',
-        }],
-        options: {
-          replacements: [
-            {
-              pattern: '<script data-main="assets/js/main" src="assets/js/lib/requirejs/require.js"></script>',
-              replacement: '<script data-main="assets/js/main.min" src="assets/js/require.min.js"></script>'
+        //Bower
+        bower:{
+            install: {
+                options: {
+                    targetDir: 'app/assets/js/lib',
+                    layoud: 'byComponent',
+                    install: true,
+                    copy: true,
+                    cleanBowerDir: true
+                }
             }
-          ]
+        },
+
+        //Sass
+        sass: {
+            dev:{
+                options: {
+                    sourceMap: true,
+                    sourceMapEmbed: false,
+                    sourceMapContents: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/assets/scss',
+                    src: ['*.{scss,sass}'],
+                    dest: 'app/assets/stylesheets',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        //Watch
+        watch: {
+            css: {
+                files: ['app/assets/scss/*.scss','app/assets/scss/plugins/*.scss'],
+                tasks: ['sass:dev']
+            }
+        },
+
+        //Uglify
+        uglify: {
+            options: {
+                preserveComments: false,
+            },
+            all: {
+                files: {
+                    'dist/assets/js/main.min.js': ['app/assets/js/main.js']
+                }
+            }
+        },
+
+        // CSSmin
+        cssmin: {
+            options: {
+                sourceMap: true
+            },
+            target: {
+                files: {
+                    'dist/assets/stylesheets/main.css': ['app/assets/stylesheets/main.css']
+                }
+            }
+        },
+
+        //Copy
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/',
+                    src: ['*.html'],
+                    dest: 'dist/'
+                }]
+            },
+            images: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/assets/imgs',
+                    src: ['**/*.{png,jpg,svg,gif}'],
+                    dest: 'dist/assets/imgs'
+                }]
+            },
+            fonts:{
+                files: [{
+                    expand: true,
+                    cwd: 'app/assets/fonts',
+                    src: ['**/*.*'],
+                    dest: 'dist/assets/fonts'
+                }]
+            },
+        },
+
+        //Replace
+        'string-replace': {
+            inline: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: '*.html',
+                    dest: 'dist/',
+                }],
+                options: {
+                    replacements: [
+                        {
+                            pattern: '<script src="/assets/js/main.js"></script>',
+                            replacement: '<script src="/assets/js/main.min.js"></script>'
+                        },
+                    ]
+                }
+            }
+        },
+
+        //BrowserSync
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        "app/assets/stylesheets/*.css",
+                        "app/**/*.html"
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './app'
+                }
+            }
         }
-      }
-    },
 
-    // require
-    requirejs: {
-      compile: {
-        options: {
-          mainConfigFile : "app/assets/js/main.js",
-          baseUrl : "app/assets/js",
-          name: "main",
-          out: "dist/assets/js/main.min.js",
-          removeCombined: true,
-          findNestedDependencies: true
-        }
-      }
-    }
 
-  });
+    });
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-bower-task');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-string-replace');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
+    // Load tasks
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-string-replace');
 
-  // Register tasks
-  grunt.registerTask('default', ['compass:dev','watch']);
-  grunt.registerTask('prod', ['compass:prod','copy','string-replace','uglify','requirejs']);
+    // Register tasks
+    grunt.registerTask('default', ['browserSync','watch']);
+    grunt.registerTask('build', ['cssmin','copy','string-replace','uglify']);
 };
